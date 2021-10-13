97561b6a804fb71db2bf737d7a43210497a14c4d
=LasseMunk <=>
2021-10-13 13:17:17 +0200

    Command Pattern

---
+++ Command Pattern/Assets/Model/Scripts/InputHandler.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+  public GameObject actor;
+  Animator anim;
+  Command keyQ, keyW, keyE, keyR, upArrow;
+  List<Command> oldCommands = new List<Command>();
+
+  Coroutine replayCoroutine;
+  bool shouldStartReplay;
+  bool isReplaying;
+
+  void Start()
+  {
+    keyQ = new PerformJump();
+    keyW = new PerformKick();
+    keyE = new PerformPunch();
+    keyR = new DoNothing();
+    upArrow = new MoveForward();
+    anim = actor.GetComponent<Animator>();
+    Camera.main.GetComponent<CameraFollow360>().player = actor.transform;
+  }
+
+  void Update()
+  {
+    if (!isReplaying) HandleInput();
+    StartReplay();
+  }
+
+  void HandleInput()
+  {
+    if (Input.GetKeyDown(KeyCode.Q))
+    {
+      keyQ.Execute(anim, true);
+      oldCommands.Add(keyQ);
+    }
+    else if (Input.GetKeyDown(KeyCode.W))
+    {
+      keyW.Execute(anim, true);
+      oldCommands.Add(keyW);
+    }
+    else if (Input.GetKeyDown(KeyCode.E))
+    {
+      keyE.Execute(anim, true);
+      oldCommands.Add(keyE);
+    }
+    else if (Input.GetKeyDown(KeyCode.R))
+    {
+      keyR.Execute(anim, true);
+      oldCommands.Add(keyR);
+    }
+    else if (Input.GetKeyDown(KeyCode.UpArrow))
+    {
+      upArrow.Execute(anim, true);
+      oldCommands.Add(upArrow);
+    }
+
+    if (Input.GetKeyDown(KeyCode.Space)) shouldStartReplay = true;
+    if (Input.GetKeyDown(KeyCode.Z)) UndoLastCommand();
+  }
+
+  void UndoLastCommand()
+  {
+    if (oldCommands.Count > 0)
+    {
+      Command c = oldCommands[oldCommands.Count - 1];
+      c.Execute(anim, false);
+      oldCommands.RemoveAt(oldCommands.Count - 1);
+    }
+  }
+
+  void StartReplay()
+  {
+    if (shouldStartReplay && oldCommands.Count > 0)
+    {
+      shouldStartReplay = false;
+      if (replayCoroutine != null)
+      {
+        StopCoroutine(replayCoroutine);
+      }
+      replayCoroutine = StartCoroutine(ReplayCommands());
+    }
+
+  }
+  IEnumerator ReplayCommands()
+  {
+    isReplaying = true;
+
+    for (int i = 0; i < oldCommands.Count; i++)
+    {
+      oldCommands[i].Execute(anim, true);
+      yield return new WaitForSeconds(1f);
+    }
+
+    isReplaying = false;
+  }
+
+}
